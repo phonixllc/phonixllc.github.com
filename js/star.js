37d0e39238ba2225660db83c5193075655666e6d
Phönix <zandernorell@gmail.com>
2013-08-06 20:36:11 -0500

    lots of new changes

--- js/star.js
+++ js/star.js
@@ -9,6 +9,49 @@ $('#summary').click(function(){
     
 });
 
+$('#testimonial').click(function(){
+    $('#hidden_infoZ').slideToggle('slow', function(){
+        if( $('#hidden_infoZ').is(':hidden') ){
+            $('#testimonial').text('Testimonials');
+        } else {
+            $('#testimonial').text('Hide content');
+        }
+    });
+    
+});
+
+$('#norellAbout').click(function(){
+    $('#hidden_infoN').slideToggle('slow', function(){
+        if( $('#hidden_infoN').is(':hidden') ){
+            $('#norellAbout').text('Artist Statement');
+        } else {
+            $('#norellAbout').text('Hide content');
+        }
+    });
+    
+});
+
+$('#norellTest').click(function(){
+    $('#hidden_infoN2').slideToggle('slow', function(){
+        if( $('#hidden_infoN2').is(':hidden') ){
+            $('#norellTest').text('Testimonials');
+        } else {
+            $('#norellTest').text('Hide content');
+        }
+    });
+    
+});
+
+$('#blogs').click(function(){
+    $('#hidden_infoBlogs').slideToggle('slow', function(){
+        if( $('#hidden_infoBlogs').is(':hidden') ){
+            $('#blogs').text('Blogs');
+        } else {
+            $('#blogs').text('Hide content');
+        }
+    });
+    
+});
 
 $(".view a").on('click', function(){
     $('.products ul').toggleClass('list');
