c8dfa8feab085ec5414287bf78d0ee3a0401f4e5
Phönix <zandernorell@gmail.com>
2014-01-18 14:31:25 -0600

    one more attempt

--- contact.php
+++ contact.php
@@ -1,6 +1,11 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> 
+<html lang="en" class="no-js"> <!--<![endif]-->
 <head>
   <meta http-equiv="Content-Type" content="charset=utf-8">
   <meta name="Designed By" content="Zander &amp; Norell">
